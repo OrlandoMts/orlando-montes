beab96f46c06a6b41c5c80bc2797a3835f61438b
OrlandoMts <dani_monts19@hotmail.com>
2022-02-24 00:27:28 -0600

    Modifique el favicon

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,7 @@ const contact = () => {
       <Head>
           <title>Contacto</title>
           <meta name="description" content="Información de contacto" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className='bg-[#f6f6f6] h-[90vh]'>
         <Contact/>
